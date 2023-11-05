35adc74cc73b66b3dbf6a32d1cf32b56ab31c196
eldrago <63483703+eldrago4@users.noreply.github.com>
2023-11-05 17:38:02 +0530

    src pkg

---
+++ Ooppro/src/main/java/vnb/ooppro/Dbapp.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package vnb.ooppro;
+/**
+ *
+ * @author sspmcoe
+ */
+public class Dbapp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
